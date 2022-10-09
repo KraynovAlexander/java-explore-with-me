06c66d1409ebe519d35121142dce43cda286b42d
Александр <KraynovAB@yandex.ru>
2022-10-10 00:10:47 +0300

    Версия 7

--- ewm-service/src/main/java/ru/practicum/emw/EmwService.java
+++ ewm-service/src/main/java/ru/practicum/emw/EmwService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Service {
+public class EmwService{
 
     public static void main(String[] args) {
-        SpringApplication.run(Service.class, args);
+        SpringApplication.run(EmwService.class, args);
     }
 
 }
