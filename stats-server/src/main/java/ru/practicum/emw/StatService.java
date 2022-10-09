15d0a45ac85205cda52717e122f082494a48628e
Александр <KraynovAB@yandex.ru>
2022-10-10 00:17:59 +0300

    Версия 8

--- stats-server/src/main/java/ru/practicum/emw/StatService.java
+++ stats-server/src/main/java/ru/practicum/emw/StatService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmwService {
+public class StatService {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmwService.class, args);
+        SpringApplication.run(StatService.class, args);
     }
 
 }
